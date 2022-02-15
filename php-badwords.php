5bf0e8d0981428fbba79afb30e64df3a8a8faa37
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-02-15 16:00:22 +0100

    case sensitive removed

--- php-badwords.php
+++ php-badwords.php
@@ -3,13 +3,13 @@
     $paragrafo = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 //con il get ottentgo la parola da censurare
-    $inputUtente = $_GET["input"];
+    $inputUtente = strtolower($_GET["input"]);
 
 //decido come verrà censurata la parola
     $censura = "***";
 
 //la nuova stringa censurata
-    $fraseCensurata = str_replace($inputUtente, $censura, $paragrafo);
+    $fraseCensurata = str_ireplace($inputUtente, $censura, $paragrafo);
 
 //stile extra per il bene dello stile
     $stile = "style='width: 80%; margin: 50px auto; text-align: center;'";
@@ -27,5 +27,6 @@
 <body>
     <h3 <?php echo $stile ?>> <?php echo $paragrafo; ?> </h3>
     <h3 <?php echo $stile ?>> <?php echo $fraseCensurata; ?> </h3>
+    <h3 <?php echo $stile ?>> <?php echo strlen($fraseCensurata); ?> </h3>
 </body>
 </html>
